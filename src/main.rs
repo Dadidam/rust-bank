866dd3c9bbeb3690e62fc6febea6c859fd7dd2ae
Ilia Vorontcov <joker.pnz@gmail.com>
2025-01-02 18:15:57 -0500

    deposit and withdraw to/from account, show bank and account summaries

--- src/main.rs
+++ src/main.rs
@@ -10,8 +10,26 @@ impl Account {
         Account {
             id,
             holder,
-            balance: 0
+            balance: 0,
         }
+    }
+
+    fn deposit(&mut self, amount: i32) -> i32 {
+        self.balance += amount;
+
+        // return the updated balance
+        self.balance
+    }
+
+    fn withdraw(&mut self, amount: i32) -> i32 {
+        self.balance -= amount;
+
+        // return the updated balance
+        self.balance
+    }
+
+    fn account_summary(&self) -> String {
+        format!("{} has a balance {}", self.holder, self.balance)
     }
 }
 
@@ -24,31 +42,45 @@ impl Bank {
     fn new() -> Self {
         Bank { accounts: vec![] }
     }
-}
 
-fn print_account(account: &Account) {
-    println!("{:#?}", account);
-}
+    fn add_account(&mut self, account: Account) {
+        self.accounts.push(account);
+    }
 
-fn change_account(account: &mut Account) {
-    account.balance = 1000;
+    fn total_balance(&self) -> i32 {
+        self.accounts.iter().map(|account| account.balance).sum()
+    }
+
+    fn bank_summary(&self) -> Vec<String> {
+        self.accounts
+            .iter()
+            .map(|account| account.account_summary())
+            .collect::<Vec<String>>()
+    }
 }
 
 fn main() {
-    // 1 - immutable way of borrowing
-    let account = Account::new(1, String::from("me"));
+    // create new bank instance
+    let mut bank = Bank::new();
 
-    // read-only reference - uses `&` operator:
-    print_account(&account);
+    // create new account instance (in immutable way of borrowing)
+    let mut account = Account::new(1, String::from("Ilia"));
 
-    println!("{:#?}", &account);
-    
-    // 2 - mutable borrowing
-    let mut new_account = Account::new(2, String::from("you"));
+    // deposit money to the account
+    account.deposit(750);
 
-    // mutable reference uses `mut` keyword along with `&` operator
-    // NOTE: you can't create a mutable ref in case there'are immutable refs currently in use
-    change_account(&mut new_account);
-    
-    println!("{:#?}", &new_account);    
+    // withdraw money from the account
+    account.withdraw(300);
+
+    // show an account summary as a string
+    account.account_summary();
+
+    // add account to the Bank
+    bank.add_account(account);
+
+    // display bank summary
+    println!("{:#?}", bank.bank_summary());
+
+    // display total balance
+    println!("{}", bank.total_balance());
 }
